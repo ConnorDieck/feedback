69b81e77ef165bd1c470432cc23196b46bc8aa8c
Connor Dieck <connordieck@gmail.com>
2021-07-19 17:02:07 -0400

    modified Python code and added python code up to step 7 and part of step 8

--- app.py
+++ app.py
@@ -1,9 +1,92 @@
-from flask import Flask, request, redirect, render_template, flash, jsonify
-from models import db, connect_db, User
+from flask import Flask, request, redirect, render_template, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User, Feedback
+from forms import RegisterForm, LoginForm
+from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///todos_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///feedback_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config["SECRET_KEY"] = "abc123"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 connect_db(app)
+
+toolbar = DebugToolbarExtension(app)
+
+@app.route("/")
+def show_home():
+    """Redirects to home page"""
+
+    return redirect('/register')
+
+@app.route("/register", methods=["GET", "POST"])
+def register_user():
+    form = RegisterForm()
+    if form.validate_on_submit():
+
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+
+        new_user = User.register(username, password, email, first_name, last_name)
+
+        db.session.add(new_user)
+        try:
+            db.session.commit()
+        except IntegrityError:
+            form.username.errors.append('Username taken. Please pick another')    
+            return render_template('register.html', form=form)
+
+        session['username'] = new_user.username
+        flash('Welcome! Successfully Created Your Account!', "success")
+        return redirect(f"/users/{new_user.username}")
+
+    return render_template('register.html', form=form)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+
+        user = User.authenticate(username, password)
+        if user:
+            flash(f"Welcome Back, {user.first_name}!", "text-primary")
+            session['username'] = user.username
+            return redirect(f'/users/{user.username}')
+        else:
+            form.username.errors = ['Invalid username/password.']
+
+    return render_template('login.html', form=form)
+
+@app.route("/users/<username>")
+def show_secret(username):
+    if 'username' not in session:
+        flash("Please login first!", "text-danger")
+        return redirect('/login')
+
+    user = User.query.get(username)
+
+    return render_template('user-details.html', user=user)
+
+@app.route("/logout")
+def logout():
+    session.pop('username')
+
+    return redirect('/')
+
+@app.route("/users/<username>/delete")
+def delete_user(username):
+    if session['username'] is not username:
+        flash("Please login as the correct user first!", "text-danger")
+        return redirect('/login')
+
+    user = User.query.get(username)
+
+    db.session.delete(user)
+    db.session.commit()
--- models.py
+++ models.py
@@ -18,8 +18,45 @@ class User(db.Model):
     
     __tablename__ = "users"
 
-    username = db.Column(db.Text(20), unique=True, nullable=False, primary_key=True)
-    password = db.Column(db.Text(), nullable=False)
-    email = db.Column(db.Text(50), nullable=False)
-    first_name = db.Column(db.Text(30), nullable=False)
-    last_name = db.Column(db.Text(30), nullable=False)
+    username = db.Column(db.String(20), unique=True, nullable=False, primary_key=True)
+    password = db.Column(db.String(), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+
+    @classmethod
+    def register(cls, username, pwd, email, first_name, last_name):
+        """Register user w/hashed password & return user."""
+
+        hashed = bcrypt.generate_password_hash(pwd)
+
+        hashed_utf8 = hashed.decode("utf8")
+
+        return cls(username=username, password=hashed_utf8, email=email, first_name=first_name, last_name=last_name)
+
+    @classmethod
+    def authenticate(cls, username, pwd):
+        """Validate that user exists & password is correct.
+        
+        Return user if valid; else return False.
+        """
+
+        u = User.query.filter_by(username=username).first()
+
+        if u and bcrypt.check_password_hash(u.password, pwd): 
+            # return user instance
+            return u
+        else:
+            return False
+
+class Feedback(db.Model):
+    """Feedback model"""
+
+    __tablename__ = "feedback"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement = True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    username = db.Column(db.Text, db.ForeignKey('users.username'),  nullable=False)
+
+    user = db.relationship('User', backref="feedback")
